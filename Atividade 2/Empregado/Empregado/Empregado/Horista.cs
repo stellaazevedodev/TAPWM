5efcf44358bdec081b15a000fb889992ae749390
Stella Azevedo <152033166+stellaazevedodev@users.noreply.github.com>
2026-03-25 18:05:44 -0300

    Add files via upload

---
+++ Atividade 2/Empregado/Empregado/Empregado/Horista.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empregado
+{
+    internal class Horista : Empregado
+    {
+        public double SalarioHora { get; set; }
+        public double NumeroHora { get; set; }
+        public int DiasFalta { get; set; }
+
+        public override double SalarioBruto()
+        {
+            return SalarioHora + NumeroHora;
+        }
+
+        public override int TempoTrabalho()
+        {
+            TimeSpan span =
+                DateTime.Today.Subtract(DataEntradaEmpresa);
+            return (span.Days - DiasFalta);
+        }
+    }
+}
