5a2145ee59cc90e5784e741a01717a9fb1e955c3
stellaazevedodev <costadeazevedostella@gmail.com>
2026-09-02 17:28:40 -0300

    Atividade 19

--- Exercicios/app.js
+++ Exercicios/app.js
@@ -1,25 +1,6 @@
-let express = require ('express');
-let app=express(); // executando o express
-
-app.set('view engine', 'ejs');
-
-app.get('/', function (req,res){
-    res.send("<html><body>Site da Fatec Sorocaba</body></html>");
-});
-
-app.get('/historia', function (req,res){
-    res.render("secao/historia");
-});
-
-app.get('/cursos', function (req,res){
-    res.render("secao/cursos");
-});
-
-app.get('/professores', function (req,res){
-    res.render("secao/professores");
-});
+let app = require('./app/config/server');
 
 app.listen(3000, function(){
-    console.log("Olá o servidor com express foi carregado com sucesso");
+    console.log('servidor iniciado');
 });
 
