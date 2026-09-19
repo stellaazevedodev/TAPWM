54e8304e79c9ffca3c893a23cd98b01004949ce3
stellaazevedodev <costadeazevedostella@gmail.com>
2026-09-18 22:04:06 -0300

    Adding database

--- Exercicios/app.js
+++ Exercicios/app.js
@@ -1,5 +1,7 @@
 let app = require('./app/config/server');
 
+require('./app/routes/professores')(app);
+
 app.listen(3000, function(){
     console.log('servidor iniciado');
 });
--- Exercicios/app/routes/professores.js
+++ Exercicios/app/routes/professores.js
@@ -1,5 +1,50 @@
 module.exports = function (app) {
+
     app.get('/informacao/professores', function (req, res) {
-        res.render("informacao/professores");
+
+        const sql = require('mssql/msnodesqlv8');
+
+        const sqlConfig = {
+
+            server: 'localhost\\SQLEXPRESS',
+
+            database: 'BD',
+
+            driver: 'ODBC Driver 18 for SQL Server',
+
+            options: {
+
+                trustedConnection: true,
+                encrypt: false,
+                trustServerCertificate: true
+
+            }
+
+        };
+
+        async function getProfessores() {
+
+            try {
+
+                const pool = await sql.connect(sqlConfig);
+
+                const results = await pool.request()
+                    .query('SELECT * FROM PROFESSORES');
+
+                res.render('informacao/professores', {
+                    profs: results.recordset
+                });
+
+            } catch (err) {
+
+                console.log(err);
+
+            }
+
+        }
+
+        getProfessores();
+
     });
+
 }
